82bed709ca49da6ba93d493250774bd7874e4ec0
dahu <barry.arthur@gmail.com>
2013-03-09 16:11:53 +0800

    rearranged for tree

--- test/test_expr9.rb
+++ test/test_expr9.rb
@@ -4,7 +4,7 @@ class TestExpr_9 < Test::Unit::TestCase
 
   def test_ints
     assert_equal([1, 123], Vimitator::Parser.new.scan_str('123'))
-    assert_equal([1, [[1, 123], [1, "+"], [1, 456]]], Vimitator::Parser.new.scan_str('123 + 456'))
+    assert_equal([1, [[1, "+"], [[1, 123], [1, 456]]]], Vimitator::Parser.new.scan_str('123 + 456'))
   end
 
 end
