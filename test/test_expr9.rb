d7efa35ccb06dab12bad5dba251b7c27d39ce2d6
dahu <barry.arthur@gmail.com>
2013-03-09 15:53:30 +0800

    cleaner cst s-exp

--- test/test_expr9.rb
+++ test/test_expr9.rb
@@ -3,8 +3,8 @@ require 'helper'
 class TestExpr_9 < Test::Unit::TestCase
 
   def test_ints
-    assert_equal([1, [123]], Vimitator::Parser.new.scan_str('123'))
-    assert_equal([1, [[1, [123]], [1, ["+"]], [1, [456]]]], Vimitator::Parser.new.scan_str('123 + 456'))
+    assert_equal([1, 123], Vimitator::Parser.new.scan_str('123'))
+    assert_equal([1, [[1, 123], [1, "+"], [1, 456]]], Vimitator::Parser.new.scan_str('123 + 456'))
   end
 
 end
